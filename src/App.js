2d38d09554dfd0a6ca2685f887c43974cd52286f
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-10-11 15:18:21 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import Topics from './components/Topics/Topics';
+import Main from './layout/Main';
+// add react router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path: '/topics',
+        element: <Topics></Topics>
+      },
+      {
+        path: '/blog',
+        element: <Blog></Blog>
+      }
+    ],
+  },
+])
 function App() {
   
   return (
-    <div className="App">
-     <h1 className='text-3xl font-bold'>Assignment is about to start.....</h1>
+    <div className="">
+     <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Bars4Icon, XMarkIcon} from '@heroicons/react/24/solid'
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <div>
-            <h3>header</h3>
-        </div>
+        <nav className='flex items-center justify-between'>
+            <div>
+            <h3 className='text-2xl font-semibold '>Quiz Arena</h3>
+            </div>
+           <div>
+           <div onClick={() => setOpen(!open)} className='h-6 w-6 md:hidden flex'>
+            {
+                open ? <XMarkIcon></XMarkIcon> : <Bars4Icon/>
+            }
+            </div>
+            <ul className={`md:flex w-full md:static absolute duration-500 ease-in ${open ? 'top-6' : 'top-[-120px]'}`}>
+                <li className='mr-6'><Link to='/'>Home</Link></li>
+                <li className='mr-6'><Link to='/statistics'>Statistics</Link></li>
+                <li className='mr-6'> <Link to='/topics'>Topics</Link></li>
+                <li className='mr-6'> <Link to='/blog'>Blog</Link></li>
+            </ul>
+           </div>
+        </nav>
     );
 };
 
